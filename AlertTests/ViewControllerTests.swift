d147c367dfd237ac64d1e624ca8b1dc09aae4068
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-19 22:11:55 +0500

    Add ViewControllerPresentationSpy and ViewControllerTests
    .ViewControllerPresentationSpy used to make alert testable
    .ViewControllerTests to test alert

---
+++ AlertTests/ViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ViewControllerTests.swift
+//  AlertTests
+//
+//  Created by HAQQQABD on 19/08/2023.
+//
+
+import XCTest
+@testable import Alert
+
+final class ViewControllerTests: XCTestCase {
+    func test_zero(){
+        XCTFail("there is no test cases are implemented in ViewControllerTests class yet")
+    }
+}
