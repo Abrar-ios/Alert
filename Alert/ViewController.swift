8bfc20d1074027b9304ca5411773054632632b8a
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-19 22:00:57 +0500

    Add Button and Implement tapping action

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private(set) var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction private func buttonTap(){
+        let alert = UIAlertController(title: "Do the thing?", message: "let us know if you want to di thing", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 
 }
 
